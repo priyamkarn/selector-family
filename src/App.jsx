9d8ed4ab039b79fc39131713c3a6dfc7fd163365
priyam karn <83950448+priyamkarn@users.noreply.github.com>
2024-08-30 21:57:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,3 +23,48 @@ function Todo({id}) {
 }
 
 export default App
+
+
+/*
+  export const fdata = selectorFamily({
+    key: 'fdata',
+    get: (id) => async () => {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
+        return res.data;
+    }
+});
+import { useRecoilValue, useRecoilValueLoadable } from 'recoil';
+import { countnetwork, countnotification, total, fdata } from './recoil/Atom';
+import './App.css';
+
+function App() {
+    const nvalue = useRecoilValue(countnetwork);
+    const jvalue = useRecoilValue(countnotification);
+    const tv = useRecoilValue(total);
+    const id = 1; // Define the ID you want to fetch data for
+    const fdataLoadable = useRecoilValueLoadable(fdata(id));
+
+    return (
+        <>
+            my network count is {nvalue}<br />
+            my notification value is {jvalue}<br />
+            my total value is {tv}<br />
+            {fdataLoadable.state === 'hasValue' ? (
+                <div>
+                    <div key={fdataLoadable.contents.id}>
+                        {fdataLoadable.contents.id}: {fdataLoadable.contents.title}
+                    </div>
+                </div>
+            ) : fdataLoadable.state === 'loading' ? (
+                <div>Loading...</div>
+            ) : (
+                <div>Error loading data</div>
+            )}
+        </>
+    );
+}
+
+export default App;
+
+*/
+
